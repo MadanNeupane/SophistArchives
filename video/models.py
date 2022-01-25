f48cae3d1e6446868031816317349838cb07a590
MadanNeupane <madanneupane09@gmail.com>
2022-01-25 05:26:53 -0600

    added search function and improvised UI

--- video/models.py
+++ video/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class Video(models.Model):
     video_title = models.CharField(max_length=200)
     video_thumbnail = models.ImageField(default='default.jpg', upload_to='thumbnails')
-    video_file = models.FileField(upload_to='videos/%y')
+    video_file = models.FileField(upload_to='videos')
+    video_posted_on = models.DateTimeField(default=timezone.now, blank=True)
     
     def __str__(self):
         return self.video_title
--- video/views.py
+++ video/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-
-import video
 from .models import Video
+from django.db.models import Q
 
 def index_page(request):
-    videos = Video.objects.all()
+    title = 'Home'
+    data = []
+    limited_videos = []
     
+    search_query = request.GET.get('search')
+
+    if search_query:
+        data = Video.objects.filter(
+            Q(video_title__icontains=search_query)).distinct().order_by('video_posted_on')
+        if len(data) > 0:
+            limited_videos = data[:3]
+        title = f'Search results for {search_query}'
+
     context = {
-        'videos': videos
+        'data': data,
+        'videos': limited_videos,
+        'title': title,
+        'search_query': search_query
     }
     
     return render(request, 'index.html', context=context)
