912e060e05755e764c92e684b965c932ed84154f
MadanNeupane <madanneupane09@gmail.com>
2022-01-26 05:41:16 -0600

    favicons, style improvise, and optimization

--- video/models.py
+++ video/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class Video(models.Model):
     video_title = models.CharField(max_length=200)
-    video_thumbnail = models.ImageField(default='default.jpg', upload_to='thumbnails')
+    video_thumbnail = models.ImageField(blank=True, null=True, upload_to='thumbnails')
     video_file = models.FileField(upload_to='videos')
     video_posted_on = models.DateTimeField(default=timezone.now, blank=True)
     
