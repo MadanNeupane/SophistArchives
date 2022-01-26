1d2f2519ac41f00c2cce97eefd668c06f715930d
MadanNeupane <madanneupane09@gmail.com>
2022-01-26 07:07:31 -0600

    heroku troubleshoot 4

--- SophistArchives/settings.py
+++ SophistArchives/settings.py
@@ -130,7 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'components/media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'components/static'),
 ]
-STATICFILES_STORAGE = 'SophistArchives.storage.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
