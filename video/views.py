6839ff19222bee62b0ab7e3af2645e8912a8f9a8
MadanNeupane <madanneupane09@gmail.com>
2022-02-13 12:29:33 -0600

    temp changes

--- video/views.py
+++ video/views.py
@@ -6,13 +6,16 @@ def index_page(request):
     title = 'Home'
     data = []
     limited_videos = []
+    search_query_length = 0
     # recently_watched = []
 
     search_query = request.GET.get('search')
+    if search_query != None:
+        search_query_length = len(search_query)
 
-    if search_query:
+    if search_query_length > 2:
         data = Video.objects.filter(
-            Q(video_title__iexact=search_query)).distinct().order_by('video_posted_on')
+            Q(video_title__icontains=search_query)).distinct().order_by('video_posted_on')
         if len(data) > 0:
             limited_videos = data[:3]
         title = f'Search results for {search_query}'
@@ -22,7 +25,18 @@ def index_page(request):
         'videos': limited_videos,
         'title': title,
         'search_query': search_query,
+        'search_query_length': search_query_length,
         # 'recently_watched': recently_watched
     }
     
     return render(request, 'index.html', context=context)
+
+
+    # search_query = request.GET.get('search')
+    # query = Q()
+
+    # if search_query:
+    #     search_strings = search_query.split('-')
+    #     q_filter = Q(video_title__icontains=search_strings[0])
+    #     for search_string in search_strings[1:]:
+    #         q_filter |= Q(video_title__icontains=search_string)
